55d0d5651122fa366726e7c5772de2d0f87e30a2
Eric-VB <eric.viniciusbasso@gmail.com>
2025-03-24 19:57:22 -0300

    feat: Retornando dados do banco de dados.

--- src/main.js
+++ src/main.js
@@ -1,20 +1,23 @@
-const express = require('express')
-const app = express()
-const PORT = 3000
+// incluir dependencia MySQL
+const mysql = require('mysql2')
 
-app.get('/', (req, response) => {
-    response.send('app get')
+// criar conexão com banco de dados
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'ecorecicla-local',
+    database: 'ecorecicla-db'
+});
+
+
+connection.connect((err) => {
+    console.log('Conexão aprovada')
 })
 
-app.post('/', (req, response) => {
-    response.send('app Post')
-})
-
-app.delete('/', (req, response) => {
-    response.send('app Delete')
-})
-
-app.listen(PORT, () => {
-    console.log(`Esse projeto esta rodando na porta ${PORT}`)
-
+connection.query('SELECT email, username, senha from login', (err, rows, fields) => {
+    if(!err){
+        console.log('Resultado: ', rows);
+    } else{
+        console.log('Consulta nao realizada')
+    }
 })
