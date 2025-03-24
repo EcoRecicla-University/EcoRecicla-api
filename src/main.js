79906ade9cfeb38b2c772330b6bb2264852b9d41
Eric-VB <eric.viniciusbasso@gmail.com>
2025-03-24 20:22:08 -0300

    feat: Vinculando backend com front part1

--- src/main.js
+++ src/main.js
@@ -1,23 +1,56 @@
 // incluir dependencia MySQL
-const mysql = require('mysql2')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const multpart = require('connect-multiparty');
+const multipart = require('connect-multiparty');
 
-// criar conexão com banco de dados
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'ecorecicla-local',
-    database: 'ecorecicla-db'
-});
+const app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const corsOptiopns = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptiopns))
 
 
-connection.connect((err) => {
-    console.log('Conexão aprovada')
+// Colocar const banco de dados
+const multipartMiddleware = multipart({ uploadDir: './uploads' })
+app.post('/upload', multipartMiddleware, (request, response) => {
+    const files = request.files;
+    console.log(files);
+    response.json({ message: files })
 })
 
-connection.query('SELECT email, username, senha from login', (err, rows, fields) => {
-    if(!err){
-        console.log('Resultado: ', rows);
-    } else{
-        console.log('Consulta nao realizada')
-    }
+app.use((err, request, response, next) => response.json({error: err.message}))
+
+app.listen(8000, () => {
+    console.log('Servidor porta 8000')
 })
+
+
+
+// const mysql = require('mysql2')
+
+// // criar conexão com banco de dados
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'ecorecicla-local',
+//     database: 'ecorecicla-db'
+// });
+
+
+// connection.connect((err) => {
+//     console.log('Conexão aprovada')
+// })
+
+// connection.query('SELECT email, username, senha from login', (err, rows, fields) => {
+//     if(!err){
+//         console.log('Resultado: ', rows);
+//     } else{
+//         console.log('Consulta nao realizada')
+//     }
+// })
