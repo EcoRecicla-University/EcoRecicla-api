4778fd2a045c045093602b3979b646479602f7a2
Eric-VB <eric.viniciusbasso@gmail.com>
2025-05-02 21:58:24 -0300

    Revert "implementação do CRUD de categorias na Main.js"
    This reverts commit d04cea824869620e2a5e2b89de748d85af31e7b4.

--- src/main.js
+++ src/main.js
@@ -202,3 +202,157 @@ app.get('/api/veiculos', async (req, res) => {
         return res.status(500).json({ error: 'Erro na consulta ao banco de dados' });
     }
 });
+
+// Buscar funcionario por ID
+app.get('/api/funcionario/:id', async (req, res) => {
+
+    try {
+        const idFuncionario = req.params.id;
+    
+        const dadosFuncionario = await ApiFuncionario.getFuncionarioById(idFuncionario)
+        res.json(dadosFuncionario);
+
+    } catch(error) {
+        console.error('Erro na consulta:', error);
+        return res.status(500).json({ error: 'Erro na consulta ao banco de dados' });
+    }
+});
+
+// Criar novo funcionario
+app.post('/api/funcionario', async (req, res) => {
+
+    const nome = req.body.Nome;
+    const dataNascimento = req.body.Data_Nascimento;
+    const cpf = req.body.cpf;
+    const rg = req.body.Telefone;
+    const estadoCivil = req.body.estadoCivil;
+    const Telefone = req.body.Telefone;
+
+    try {
+
+        idFuncionario.criarNovoFuncionario(nome, dataNascimento, cpf, rg, estadoCivil, Telefone)
+        res.status(200).json({ success: true })
+
+    } catch(error) {
+        console.error('Erro ao inserir novo funcionario:', error);
+
+        const message = error.message ?? 'Erro ao inserir novo funcionario'
+        return res.status(500).json({ error: message });
+    }
+});
+
+// Editar funcionario
+app.put('/api/funcionario/:id', async (req, res) => {
+    const id = req.params.id
+    const nome = req.body.Nome;
+    const dataNascimento = req.body.Data_Nascimento;
+    const cpf = req.body.cpf;
+    const rg = req.body.Telefone;
+    const estadoCivil = req.body.estadoCivil;
+    const Telefone = req.body.Telefone;
+
+    try {
+        idFuncionario.editarFuncionario(id, nome, dataNascimento, cpf, rg, estadoCivil, Telefone)
+        res.status(200).json({ success: true })
+    } catch(error) {
+        console.error('Erro ao editar funcionario:', error);
+
+        const message = error.message ?? 'Erro ao editar funcionario'
+        return res.status(500).json({ error: message });
+    }
+});
+
+// Excluir funcionario
+app.delete('/api/funcionario/:id', async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        apiCliente.excluirFuncionario(id)
+        res.status(200).json({ success: true })
+    } catch(error){
+        console.error('Erro ao excluir funcionario:', error);
+
+        const message = error.message ?? 'Erro ao excluir funcionario'
+        return res.status(500).json({ error: message });
+    }
+});
+
+// Centros -----------------------------------------------------------------------------------------------------------------------
+
+// Buscar todos os centros
+app.get('/api/centro', async (req, res) => {
+    try {
+        const funcionario = await ApiCentros.listarTodos()
+        res.json(funcionario);
+
+    } catch(error) {
+        console.error('Erro na consulta:', error);
+        return res.status(500).json({ error: 'Erro na consulta ao banco de dados' });
+    }
+});
+
+// Buscar centro por ID
+app.get('/api/centro/:id', async (req, res) => {
+
+    try {
+        const idCentro = req.params.id;
+    
+        const dadosCentros = await ApiCentros.getCentroById(idCentro)
+        res.json(dadosCentros);
+
+    } catch(error) {
+        console.error('Erro na consulta:', error);
+        return res.status(500).json({ error: 'Erro na consulta ao banco de dados' });
+    }
+});
+
+// Criar novo centro
+app.post('/api/centro', async (req, res) => {
+
+    const endereco = req.body.Endereço;
+    const capaciArmaze = req.body.Capaci_Armaze;
+
+    try {
+
+        idCentro.criarNovoCentro(endereco, capaciArmaze)
+        res.status(200).json({ success: true })
+
+    } catch(error) {
+        console.error('Erro ao inserir novo centro:', error);
+
+        const message = error.message ?? 'Erro ao inserir novo centro'
+        return res.status(500).json({ error: message });
+    }
+});
+
+// Editar centro
+app.put('/api/centro/:id', async (req, res) => {
+    const id = req.params.id
+    const endereco = req.body.Endereço;
+    const capaciArmaze = req.body.Capaci_Armaze;
+
+    try {
+        idCentro.editarCentro(id, endereco, capaciArmaze)
+        res.status(200).json({ success: true })
+    } catch(error) {
+        console.error('Erro ao editar centro:', error);
+
+        const message = error.message ?? 'Erro ao editar centro'
+        return res.status(500).json({ error: message });
+    }
+});
+
+// Excluir centro
+app.delete('/api/centro/:id', async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        ApiCentros.excluirCentro(id)
+        res.status(200).json({ success: true })
+    } catch(error){
+        console.error('Erro ao excluir centro:', error);
+
+        const message = error.message ?? 'Erro ao excluir centro'
+        return res.status(500).json({ error: message });
+    }
+});
