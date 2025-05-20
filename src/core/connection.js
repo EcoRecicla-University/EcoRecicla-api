295e4f657afa7429ad6922b3b9c6e8c12eae3b19
Kelvin <kelvin_rosa@terra.com.br>
2025-05-20 20:03:21 -0300

    Update _enha_banco

--- src/core/connection.js
+++ src/core/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'ecorecicla-local',
+    password: '@91Rosa2010@',
     database: 'ecorecicla-db'
 });
 
